4484786c72f90cfffb0bbe44d046dd73c747795c
Lauren Ryan <lxryan@live.com>
2013-04-06 18:39:42 -0400

    Local DB and models mapped to it

--- Cookbook/App_Start/RouteConfig.cs
+++ Cookbook/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Cookbook
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Cookbook", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- Cookbook/Models/CookbookModels.cs
+++ Cookbook/Models/CookbookModels.cs
@@ -1,98 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
 
-namespace Cookbook.Models
-{
-    public class Cookbook
-    {
-        public int UserID { get; set; }
+//namespace Cookbook.Models
+//{
+//    public class Cookbook
+//    {
+//        public int UserID { get; set; }
 
-        //your own uploaded recipes
-        public List<Recipe> Recipes { get; set; }
+//        //your own uploaded recipes
+//        public List<Recipe> Recipes { get; set; }
 
-        //other user's recipes you've attached to your cookbook
-        public List<Recipe> Favorites { get; set; }
+//        //other user's recipes you've attached to your cookbook
+//        public List<Recipe> Favorites { get; set; }
 
-        //blog (non-recipe) posts
-        public List<BlogPost> Posts { get; set; }
+//        //blog (non-recipe) posts
+//        public List<BlogPost> Posts { get; set; }
 
-    }
+//    }
 
-    public class Recipe
-    {
-        public int RecipeID { get; set; }
-        public int UserID { get; set; }
+//    public class Recipe
+//    {
+//        public int RecipeID { get; set; }
+//        public int UserID { get; set; }
 
-        public string Title { get; set; }
+//        public string Title { get; set; }
 
-        //look up ingredient by name
-        public Dictionary<string, Ingredient> Ingredients { get; set; }
+//        //look up ingredient by name
+//        public Dictionary<string, Ingredient> Ingredients { get; set; }
         
-        public string Instructions { get; set; }
+//        public string Instructions { get; set; }
 
-        public DateTime DateCreated { get; set; }
-        public DateTime DateModified { get; set; }
+//        public DateTime DateCreated { get; set; }
+//        public DateTime DateModified { get; set; }
 
-        public List<Image> PhotoAlbum { get; set; }
+//        public List<Image> PhotoAlbum { get; set; }
 
-        public HashSet<string> Tags { get; set; }
+//        public HashSet<string> Tags { get; set; }
 
-        //look up comment by comment ID
-        public Dictionary<int, Comment> Comments { get; set; }
-        public int FavoriteCount { get; set; }
-        public string Favoriters { get; set; }
-        public int LikeCount { get; set; }
-        public string Likers { get; set; }
-    }
+//        //look up comment by comment ID
+//        public Dictionary<int, Comment> Comments { get; set; }
+//        public int FavoriteCount { get; set; }
+//        public string Favoriters { get; set; }
+//        public int LikeCount { get; set; }
+//        public string Likers { get; set; }
+//    }
 
-    public class Ingredient
-    {
-        public string Quantity { get; set; }
-        public string IngredientName { get; set; }
-    }
+//    public class Ingredient
+//    {
+//        public string Quantity { get; set; }
+//        public string IngredientName { get; set; }
+//    }
 
-    public class BlogPost
-    {
-        public int BlogPostID { get; set; }
-        public int UserID { get; set; }
+//    public class BlogPost
+//    {
+//        public int BlogPostID { get; set; }
+//        public int UserID { get; set; }
 
-        public string Title { get; set; }
-        public string Post { get; set; }
-        public List<Image> PhotoAlbum { get; set; }
+//        public string Title { get; set; }
+//        public string Post { get; set; }
+//        public List<Image> PhotoAlbum { get; set; }
 
-        public DateTime DateCreated { get; set; }
-        public DateTime DateModified { get; set; }
+//        public DateTime DateCreated { get; set; }
+//        public DateTime DateModified { get; set; }
 
-        public HashSet<string> Tags { get; set; }
+//        public HashSet<string> Tags { get; set; }
 
-        //look up comment by comment ID
-        public Dictionary<int, Comment> Comments { get; set; }
-        public string Likers { get; set; }
-        public int LikeCount { get; set; }
-    }
+//        //look up comment by comment ID
+//        public Dictionary<int, Comment> Comments { get; set; }
+//        public string Likers { get; set; }
+//        public int LikeCount { get; set; }
+//    }
 
-    public class Image
-    {
-        public int ImageID { get; set; }
-        public int UserID { get; set; }
-        public string ImageUrl { get; set; }
-        public string ImageTitle { get; set; }
-        public string ImageDescription { get; set; }
+//    public class Image
+//    {
+//        public int ImageID { get; set; }
+//        public int UserID { get; set; }
+//        public string ImageUrl { get; set; }
+//        public string ImageTitle { get; set; }
+//        public string ImageDescription { get; set; }
 
-        public DateTime DateCreated { get; set; }
-        public DateTime DateModified { get; set; }
+//        public DateTime DateCreated { get; set; }
+//        public DateTime DateModified { get; set; }
 
-        public HashSet<string> Tags { get; set; }
-    }
+//    }
 
-    public class Comment
-    {
-        public int CommentID { get; set; }
-        public int UserID { get; set; }
-        public string Content { get; set; }
-        public DateTime DateCreated { get; set; }
-    }
+//    public class Comment
+//    {
+//        public int CommentID { get; set; }
+//        public int UserID { get; set; }
+//        public string Content { get; set; }
+//        public DateTime DateCreated { get; set; }
+//    }
 
-}
+//}
