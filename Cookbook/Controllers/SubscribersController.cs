08af10daa5be9bd081ded65246e2de1b56c177fb
Lauren Ryan <lxryan@live.com>
2013-04-10 10:35:04 -0400

    created controller for subscribers

---
+++ Cookbook/Controllers/SubscribersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cookbook.Controllers
+{
+    public class SubscribersController : Controller
+    {
+
+        //View who is subscribed to you and who you are subscribed to.
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Subscribe(string username)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Unsubscribe(string username)
+        {
+            return View();
+        }
+
+    }
+}
