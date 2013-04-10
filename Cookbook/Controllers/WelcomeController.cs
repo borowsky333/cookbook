5110f91ac37e10f37d2d8060ee0f7124a206017c
shaq1nj <shaq1nj@aol.com>
2013-04-10 11:16:35 -0400

    Made more interface changes throughout.

---
+++ Cookbook/Controllers/WelcomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cookbook.Controllers
+{
+    public class NewsFeedController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult FilterByTag(string tag)
+        {
+            return View();
+        }
+
+
+
+    }
+}
