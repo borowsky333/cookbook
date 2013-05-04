d8985234bd7478a79741a863aab791434f7c9bef
Matt <borowsky333@gmail.com>
2013-05-04 17:29:37 -0400

    Added Subscribers

---
+++ Cookbook/Controllers/SubscribeController.cs
@@ -0,0 +1,91 @@
+﻿using Cookbook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Cookbook.Controllers
+{
+    public class SubscribeController : Controller
+    {
+        private CookbookDBModelsDataContext db = new CookbookDBModelsDataContext();
+        private UsersContext userDb = new UsersContext();
+
+        public ActionResult Index()
+        {
+            var subscribedUserIds = (from subscribers in db.User_Subscribers
+                         where subscribers.UserId == (int)Membership.GetUser().ProviderUserKey
+                         select subscribers.SubscriberId).ToList();
+
+            Dictionary<int, string> subscribedUsers = new Dictionary<int, string>();
+            foreach (int id in subscribedUserIds)
+            {
+                subscribedUsers[id] = ((from userprofiles in userDb.UserProfiles
+                                        where userprofiles.UserId == id
+                                        select userprofiles.UserName).FirstOrDefault());
+            }
+
+            //subscribedUsers.OrderBy(
+
+            ViewBag.SubscribedUsers = subscribedUsers;
+            return View();
+        }
+
+        
+
+
+        //[HttpPost]
+        public ActionResult AddSubscriber(int userId)
+        {
+            if (userId == (int)Membership.GetUser().ProviderUserKey)
+            {
+                ViewBag.Message = "You cannot follow yourself";
+                return View();
+            }
+
+
+            User_Subscriber us = new User_Subscriber
+            {
+                UserId = (int)Membership.GetUser().ProviderUserKey,
+                SubscriberId = userId
+            };
+
+            
+
+            if(db.User_Subscribers.Contains(us))
+            {
+                ViewBag.Message = "You were already following this user";
+                return View();
+            }
+
+            db.User_Subscribers.InsertOnSubmit(us);
+            db.SubmitChanges();
+
+            ViewBag.Message = "Subscribed";
+
+            return RedirectToAction("Index");
+        }
+
+        //[HttpPost]
+        public ActionResult RemoveSubscriber(int userId)
+        {
+            User_Subscriber us = new User_Subscriber
+            {
+                UserId = (int)Membership.GetUser().ProviderUserKey,
+                SubscriberId = userId
+            };
+
+            db.User_Subscribers.DeleteOnSubmit(us);
+            db.SubmitChanges();
+
+            ViewBag.Message("Unsubscribed");
+            return RedirectToAction("Index");
+
+        }
+
+
+
+    }
+}
