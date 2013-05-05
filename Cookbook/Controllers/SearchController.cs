908c1ea4aa9776f1c60ec7f49dd9029093464d73
Matt <borowsky333@gmail.com>
2013-05-05 16:11:20 -0400

    Modified Search Controller

--- Cookbook/Controllers/SearchController.cs
+++ Cookbook/Controllers/SearchController.cs
@@ -12,36 +12,34 @@ namespace Cookbook.Controllers
         private CookbookDBModelsDataContext db = new CookbookDBModelsDataContext();
         private UsersContext userDb = new UsersContext();
 
-        public ActionResult SearchUsers(string userName)
+        public ActionResult SearchUsers(string query)
         {
-            List<UserProfile> result = (from allUsers in userDb.UserProfiles
-                                        where allUsers.UserName.Contains(userName)
+            //Search Tags
+            List<UserProfile> UserResults = (from allUsers in userDb.UserProfiles
+                                        where allUsers.UserName.Contains(query)
                                         select allUsers).ToList();
 
-            UserProfile exactMatch = result.Find(delegate(UserProfile up)
+            UserProfile exactMatch = UserResults.Find(delegate(UserProfile up)
                                     {
                                         return string.Equals(up.UserName,
-                                                             userName,
+                                                             query,
                                                              StringComparison.OrdinalIgnoreCase);
                                     });
 
             if (exactMatch != null)//if there is an exact match, move it to the top of the list
             {
-                result.Remove(exactMatch);
-                result.Insert(0, exactMatch);
+                UserResults.Remove(exactMatch);
+                UserResults.Insert(0, exactMatch);
             }
 
-            ViewBag.UserResult = result;
-            return View();
-        }
-
-        public ActionResult SearchTags(string tag)
-        {
-            List<Recipe> results = (from recipes in db.Recipes
-                                    where recipes.Recipe_Tags.Contains(new Recipe_Tag { Tag = tag }, new TagComparer())
+            ViewBag.UserResults = UserResults;
+            
+            //Search Users
+            List<Recipe> TagResults = (from recipes in db.Recipes
+                                    where recipes.Recipe_Tags.Contains(new Recipe_Tag { Tag = query }, new TagComparer())
                                     select recipes).ToList();
 
-            ViewBag.TagResults = results;
+            ViewBag.TagResults = TagResults;
 
             return View();
         }
@@ -49,11 +47,16 @@ namespace Cookbook.Controllers
     }
 
 
-    private class TagComparer : IEqualityComparer<Recipe_Tag>
+    public class TagComparer : IEqualityComparer<Recipe_Tag>
     {
-        public bool equals(Recipe_Tag t1, Recipe_Tag t2)
+        public bool Equals(Recipe_Tag t1, Recipe_Tag t2)
         {
             return string.Equals(t1.Tag,t2.Tag,StringComparison.OrdinalIgnoreCase);
+        }
+
+        public int GetHashCode(Recipe_Tag tag)
+        {
+            return tag.Tag.GetHashCode();
         }
     }
 }
